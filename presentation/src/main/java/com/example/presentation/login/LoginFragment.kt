5c40a816d1805ad175e44476e409520a8eda43fc
jimin <jiminn821@naver.com>
2023-03-22 18:48:40 +0900

    feat : 로그인 버튼 클릭시 모의 면접으로 이동 구현

--- presentation/src/main/java/com/example/presentation/login/LoginFragment.kt
+++ presentation/src/main/java/com/example/presentation/login/LoginFragment.kt
@@ -6,13 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
+import com.example.presentation.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding
+        get() = _binding!!
 
     private lateinit var viewModel: LoginViewModel
 
@@ -20,7 +22,19 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            val navController = findNavController()
+            navController.setGraph(R.navigation.nav_graph)
+            navController.navigate(R.id.action_loginFragment_to_interviewFragment)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
