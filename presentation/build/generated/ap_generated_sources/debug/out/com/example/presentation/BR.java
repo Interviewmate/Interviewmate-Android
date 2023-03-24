e5da3ff70b2700dce51e3b6fe9901ecb6cdedc17
jimin <jiminn821@naver.com>
2023-03-24 13:49:55 +0900

    feat : gitignore 수정

--- presentation/build/generated/ap_generated_sources/debug/out/com/example/presentation/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.presentation;
-
-public class BR {
-  public static int _all = 0;
-}
--- presentation/build/generated/data_binding_trigger/debug/com/example/presentation/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.presentation;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
