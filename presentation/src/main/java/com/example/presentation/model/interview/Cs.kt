5fe86c37f90364a61cf88da411f259f8be39ca5d
jimin <jiminn821@naver.com>
2023-04-13 02:11:56 +0900

    feat : keyword 화면 chipGroup 구현

---
+++ presentation/src/main/java/com/example/presentation/model/interview/Cs.kt
@@ -0,0 +1,10 @@
+package com.example.presentation.model.interview
+
+enum class Cs(val text: String) {
+    DATA_STRUCTURE("자료구조"),
+    ALGORITHM("알고리즘"),
+    COMPUTER_ARCHITECTURE("컴퓨터 구조"),
+    OPERATING_SYSTEM("운영체제"),
+    NETWORK("네트워크"),
+    DATABASE("데이터 베이스")
+}
--- presentation/src/main/java/com/example/presentation/ui/interview/KeywordFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/interview/KeywordFragment.kt
@@ -1,13 +1,22 @@
 package com.example.presentation.ui.interview
 
+import android.content.Context
+import android.content.res.ColorStateList
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.presentation.MainActivity
+import com.example.presentation.R
 import com.example.presentation.databinding.FragmentJobSkillBinding
+import com.example.presentation.model.interview.Cs
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class KeywordFragment : Fragment() {
     private var _binding: FragmentJobSkillBinding? = null
@@ -17,6 +26,9 @@ class KeywordFragment : Fragment() {
     private lateinit var mainActivity: MainActivity
 
     private lateinit var viewModel: KeywordViewModel
+
+    private lateinit var contentChipGroup: ChipGroup
+    private var chipId = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +44,53 @@ class KeywordFragment : Fragment() {
 
         mainActivity = activity as MainActivity
         mainActivity.hideBottomNavi(true)
+
+        chipId = 0
+
+        setSkills()
+    }
+
+    private fun setSkills() {
+        binding.tvHeader.text = getString(R.string.select_keyword)
+        binding.tvDescription.text = getString(R.string.select_keyword_description)
+        addLayout()
+    }
+
+    private fun addLayout() {
+        val layoutInflater =
+            activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val containView = layoutInflater.inflate(R.layout.item_skills, null)
+        binding.layoutSkills.addView(containView)
+
+        val contentView = containView as View
+        (contentView.findViewById(R.id.tv_skill_title) as TextView).text = getString(R.string.cs)
+
+        contentChipGroup = contentView.findViewById(R.id.chip_group) as ChipGroup
+        contentChipGroup.isSingleSelection = false
+        Cs.values().forEach {
+            addChip(it.text, contentChipGroup)
+        }
+    }
+
+    private fun addChip(skillName: String, chipGroup: ChipGroup?) {
+        chipGroup?.addView(Chip(context).apply {
+            text = skillName
+            id = chipId++
+            setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16F)
+            isCheckable = true
+            isCheckedIconVisible = false
+            chipBackgroundColor = ColorStateList(
+                arrayOf(
+                    intArrayOf(-android.R.attr.state_checked),
+                    intArrayOf(android.R.attr.state_checked)
+                ),
+                intArrayOf(
+                    ContextCompat.getColor(context, R.color.medium_gray),
+                    ContextCompat.getColor(context, R.color.yellow)
+                )
+            )
+            setTextColor(ContextCompat.getColor(context, R.color.white))
+        })
     }
 
     override fun onDestroyView() {
@@ -39,6 +98,7 @@ class KeywordFragment : Fragment() {
 
         super.onDestroyView()
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(KeywordViewModel::class.java)
