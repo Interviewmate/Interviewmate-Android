031ce65e97b575ecd83fa08ef4adc68ea977c72a
jimin <jiminn821@naver.com>
2023-04-13 01:46:55 +0900

    feat : keyword 화면에서 바텀시트 안보이도록 구현

--- presentation/src/main/java/com/example/presentation/ui/interview/KeywordFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/interview/KeywordFragment.kt
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.presentation.MainActivity
 import com.example.presentation.databinding.FragmentJobSkillBinding
 
 class KeywordFragment : Fragment() {
     private var _binding: FragmentJobSkillBinding? = null
     private val binding: FragmentJobSkillBinding
         get() = _binding!!
+
+    private lateinit var mainActivity: MainActivity
 
     private lateinit var viewModel: KeywordViewModel
 
@@ -24,6 +27,18 @@ class KeywordFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainActivity = activity as MainActivity
+        mainActivity.hideBottomNavi(true)
+    }
+
+    override fun onDestroyView() {
+        mainActivity.hideBottomNavi(false)
+
+        super.onDestroyView()
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(KeywordViewModel::class.java)
