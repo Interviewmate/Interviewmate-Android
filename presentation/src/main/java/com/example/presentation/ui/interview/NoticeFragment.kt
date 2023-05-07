d6200bef537d492a9ba9dd2bbd961a1e981bb6ab
jeeminimini <jiminn821@naver.com>
2023-05-07 19:19:52 +0900

    feat : tedPermission을 이용한 camera, audio 권한 허용

--- presentation/src/main/java/com/example/presentation/ui/interview/NoticeFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/interview/NoticeFragment.kt
@@ -1,19 +1,23 @@
 package com.example.presentation.ui.interview
 
+import android.Manifest
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentNoticeBinding
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+
 
 class NoticeFragment : Fragment() {
     private var _binding: FragmentNoticeBinding? = null
     private val binding: FragmentNoticeBinding
         get() = _binding!!
 
+    private var isPermission = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,8 +30,29 @@ class NoticeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        TedPermission.create()
+            .setPermissionListener(permissionlistener)
+            .setRationaleMessage("녹화를 위하여 권한을 허용해주세요.")
+            .setDeniedMessage("권한이 거부되었습니다. 설정 > 권한에서 허용해주세요.")
+            .setPermissions(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+            .check();
+
         binding.btnNext.setOnClickListener {
-            findNavController().navigate(R.id.action_noticeFragment_to_recordFragment)
+            if (isPermission) {
+                findNavController().navigate(R.id.action_noticeFragment_to_recordFragment)
+            }
+        }
+    }
+
+    var permissionlistener: PermissionListener = object : PermissionListener {
+        override fun onPermissionGranted() {
+            Toast.makeText(context, "권한 허가", Toast.LENGTH_SHORT).show()
+            isPermission = true
+        }
+
+        override fun onPermissionDenied(deniedPermissions: List<String>) {
+            Toast.makeText(context, "권한 거부", Toast.LENGTH_SHORT).show()
+            isPermission = false
         }
     }
 }
--- presentation/src/main/java/com/example/presentation/ui/interview/RecordFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/interview/RecordFragment.kt
@@ -1,9 +1,10 @@
 package com.example.presentation.ui.interview
 
+import android.hardware.Camera
+import android.media.MediaRecorder
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -13,12 +14,20 @@ import com.example.presentation.databinding.FragmentRecordBinding
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
+
 class RecordFragment : Fragment() {
     private var _binding: FragmentRecordBinding? = null
     private val binding: FragmentRecordBinding
         get() = _binding!!
 
     private val recordViewModel: RecordViewModel by viewModels()
+
+    private val camera: Camera? = null
+    private val mediaRecorder: MediaRecorder? = null
+    private val btn_record: Button? = null
+    private val surfaceView: SurfaceView? = null
+    private val surfaceHolder: SurfaceHolder? = null
+    private val recording = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
