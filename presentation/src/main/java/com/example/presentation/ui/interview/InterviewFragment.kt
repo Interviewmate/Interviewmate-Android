73697402f9095be833e2f7278314396ee54ef56c
jeeminimini <jiminn821@naver.com>
2023-05-20 16:34:14 +0900

    feat : interview 생성 기능 구현

--- presentation/src/main/java/com/example/presentation/ui/interview/InterviewFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/interview/InterviewFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentInterviewBinding
+import com.example.presentation.ui.MainViewModel
+import kotlinx.coroutines.launch
 
 class InterviewFragment : Fragment() {
     private var _binding: FragmentInterviewBinding? = null
@@ -16,6 +21,7 @@ class InterviewFragment : Fragment() {
         get() = _binding!!
 
     private val interviewViewModel: InterviewViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,8 +35,23 @@ class InterviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnDoInterview.setOnClickListener{
-            findNavController().navigate(R.id.action_interviewFragment_to_keywordFragment)
+        setInterview()
+    }
+
+    private fun setInterview() {
+        binding.btnDoInterview.setOnClickListener {
+            viewLifecycleOwner.lifecycleScope.launch {
+                interviewViewModel.setInterview(mainViewModel.userAuth)
+                interviewViewModel.isInterviewMade.collect { isInterviewMade ->
+                    if (isInterviewMade) {
+                        findNavController().navigate(R.id.action_interviewFragment_to_keywordFragment)
+                    } else {
+                        Toast.makeText(requireContext(), "인터뷰 생성에 실패하였습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+
         }
     }
 
--- presentation/src/main/java/com/example/presentation/ui/interview/InterviewViewModel.kt
+++ presentation/src/main/java/com/example/presentation/ui/interview/InterviewViewModel.kt
@@ -1,11 +1,38 @@
 package com.example.presentation.ui.interview
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.domain.model.signup.UserAuth
+import com.example.domain.usecase.interview.SetInterviewUseCase
+import com.example.presentation.model.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InterviewViewModel @Inject constructor() : ViewModel() {
-    lateinit var userAuth: UserAuth
+class InterviewViewModel @Inject constructor(
+    private val setInterviewUseCase: SetInterviewUseCase
+) : ViewModel() {
+
+    private val _isInterviewMade = MutableSharedFlow<Boolean>()
+    val isInterviewMade = _isInterviewMade
+    suspend fun setInterview(userAuth: UserAuth) {
+        viewModelScope.launch {
+            setInterviewUseCase(userAuth)
+                .catch {
+                    _isInterviewMade.emit(false)
+                }
+                .collectLatest { interviewResponse ->
+                    if (interviewResponse.status == Status.SUCCESS.name) {
+                        _isInterviewMade.emit(true)
+                    } else {
+                        _isInterviewMade.emit(false)
+                    }
+                }
+        }
+    }
 }
