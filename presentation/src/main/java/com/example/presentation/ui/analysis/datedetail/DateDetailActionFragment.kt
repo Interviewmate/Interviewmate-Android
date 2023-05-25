06a925fe33b29b532930f586cbfd5241b6f944ee
jeeminimini <jiminn821@naver.com>
2023-05-26 01:42:25 +0900

    feat : exoPlayer 이용해서 인터뷰 영상 보여주기 구현
    - recyclerView 이용

--- presentation/src/main/java/com/example/presentation/ui/analysis/datedetail/DateDetailActionFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/analysis/datedetail/DateDetailActionFragment.kt
@@ -5,53 +5,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.presentation.R
+import androidx.lifecycle.lifecycleScope
+import com.example.presentation.databinding.FragmentDateDetailActionBinding
+import com.example.presentation.model.analysis.InterviewVideo
+import kotlinx.coroutines.launch
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DateDetailActionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 class DateDetailActionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentDateDetailActionBinding? = null
+    private val binding: FragmentDateDetailActionBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date_detail_action, container, false)
+    ): View {
+        _binding = FragmentDateDetailActionBinding.inflate(layoutInflater)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DateDetailActionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DateDetailActionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView()
     }
+
+    private fun setRecyclerView() {
+        val dateAnalysisListAdapter =
+            DateDetailActionListAdapter(requireContext(), requireActivity())
+        binding.recyclerViewVideo.adapter = dateAnalysisListAdapter
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            dateAnalysisListAdapter.submitList(
+                listOf(
+                    InterviewVideo(
+                        "OSI 7계층이란?",
+                        "https://interviewmate.s3.ap-northeast-2.amazonaws.com/interview/2/0493ed8d-de26-4dac-bc4b-3f6140cdbe1c"
+                    ),
+                    InterviewVideo(
+                        "페이지 로드 시간을 줄이는 세 가지 방법에 관해 이야기 해 보세요.",
+                        "https://interviewmate.s3.ap-northeast-2.amazonaws.com/interview/2/0493ed8d-de26-4dac-bc4b-3f6140cdbe1c"
+                    ),
+                )
+            )
+        }
+    }
+
+
 }
