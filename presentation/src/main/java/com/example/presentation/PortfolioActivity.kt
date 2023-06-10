312990e66e3ecad579b72a1b6187ccadb9dd89bf
jeeminimini <jiminn821@naver.com>
2023-06-10 13:00:40 +0900

    refactor : PortfolioActivity에서 lifecycleOwner 설정

--- presentation/src/main/java/com/example/presentation/PortfolioActivity.kt
+++ presentation/src/main/java/com/example/presentation/PortfolioActivity.kt
@@ -1,7 +1,6 @@
 package com.example.presentation
 
 import android.content.Intent
-import android.graphics.Paint
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -31,11 +30,15 @@ class PortfolioActivity : AppCompatActivity() {
         binding = ActivityPortfolioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.portfolioViewModel = portfolioRegisterViewModel
-
+        initBinding()
         setUserAuth()
         getPortfolioFile()
         checkPortfolioSuccess()
+    }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this@PortfolioActivity
+        binding.portfolioViewModel = portfolioRegisterViewModel
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -69,7 +72,6 @@ class PortfolioActivity : AppCompatActivity() {
                         portfolioRegisterViewModel.portfolioUri = uri.toString()
                         lifecycleScope.launch {
                             portfolioRegisterViewModel.putPortfolio(file)
-                            portfolioRegisterViewModel.getPortfolioExist(portfolioRegisterViewModel.userAuth)
                         }
                     }
                 }
